3f4c0670a7b1e91999f4aa0994eff85f709e8f47
Talha Ahmad <52959810+7alhaAhmad@users.noreply.github.com>
2022-01-01 18:49:24 +0500

    01 18 49

--- ab3.py
+++ ab3.py
@@ -88,26 +88,29 @@ octDataset = dataset4.loc[dataset4['Month'] == 'October']
 novDataset = dataset4.loc[dataset4['Month'] == 'November']
 decDataset = dataset4.loc[dataset4['Month'] == 'December']
 
-janTempMean=janDataset['Temperature (Celsius)'].mean(); janRainMean=janDataset['Rainfall (MM)'].mean()
-febTempMean=febDataset['Temperature (Celsius)'].mean(); febRainMean=febDataset['Rainfall (MM)'].mean()
-marTempMean=marDataset['Temperature (Celsius)'].mean(); marRainMean=marDataset['Rainfall (MM)'].mean()
-aprTempMean=aprDataset['Temperature (Celsius)'].mean(); aprRainMean=aprDataset['Rainfall (MM)'].mean()
-mayTempMean=mayDataset['Temperature (Celsius)'].mean(); mayRainMean=mayDataset['Rainfall (MM)'].mean()
-junTempMean=junDataset['Temperature (Celsius)'].mean(); junRainMean=junDataset['Rainfall (MM)'].mean()
-julTempMean=julDataset['Temperature (Celsius)'].mean(); julRainMean=julDataset['Rainfall (MM)'].mean()
-augTempMean=augDataset['Temperature (Celsius)'].mean(); augRainMean=augDataset['Rainfall (MM)'].mean()
-sepTempMean=sepDataset['Temperature (Celsius)'].mean(); sepRainMean=sepDataset['Rainfall (MM)'].mean()
-octTempMean=octDataset['Temperature (Celsius)'].mean(); octRainMean=octDataset['Rainfall (MM)'].mean()
-novTempMean=novDataset['Temperature (Celsius)'].mean(); novRainMean=novDataset['Rainfall (MM)'].mean()
-decTempMean=decDataset['Temperature (Celsius)'].mean(); decRainMean=decDataset['Rainfall (MM)'].mean()
+# Calculating Mean Temprature and Rain by Month.
+janTempMean=janDataset['Temperature (Celsius)'].mean();    janRainMean=janDataset['Rainfall (MM)'].mean()
+febTempMean=febDataset['Temperature (Celsius)'].mean();    febRainMean=febDataset['Rainfall (MM)'].mean()
+marTempMean=marDataset['Temperature (Celsius)'].mean();    marRainMean=marDataset['Rainfall (MM)'].mean()
+aprTempMean=aprDataset['Temperature (Celsius)'].mean();    aprRainMean=aprDataset['Rainfall (MM)'].mean()
+mayTempMean=mayDataset['Temperature (Celsius)'].mean();    mayRainMean=mayDataset['Rainfall (MM)'].mean()
+junTempMean=junDataset['Temperature (Celsius)'].mean();    junRainMean=junDataset['Rainfall (MM)'].mean()
+julTempMean=julDataset['Temperature (Celsius)'].mean();    julRainMean=julDataset['Rainfall (MM)'].mean()
+augTempMean=augDataset['Temperature (Celsius)'].mean();    augRainMean=augDataset['Rainfall (MM)'].mean()
+sepTempMean=sepDataset['Temperature (Celsius)'].mean();    sepRainMean=sepDataset['Rainfall (MM)'].mean()
+octTempMean=octDataset['Temperature (Celsius)'].mean();    octRainMean=octDataset['Rainfall (MM)'].mean()
+novTempMean=novDataset['Temperature (Celsius)'].mean();    novRainMean=novDataset['Rainfall (MM)'].mean()
+decTempMean=decDataset['Temperature (Celsius)'].mean();    decRainMean=decDataset['Rainfall (MM)'].mean()
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+avgTemps = [janTempMean,febTempMean,marTempMean,aprTempMean,mayTempMean,junTempMean,julTempMean,augTempMean,sepTempMean,octTempMean,novTempMean,decTempMean]
+avgRain = [janRainMean,febRainMean,marRainMean,aprRainMean,mayRainMean,junRainMean,julRainMean,augRainMean,sepRainMean,octRainMean,novRainMean,decRainMean]
 
 # Pakistan's Average Temprature/Month from 1901-2016
 def avgTMG():
     fig = plt.figure()
-    ax = fig.add_axes([0.1,0.1,0.8,0.8])
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    avgTemps = [janTempMean,febTempMean,marTempMean,aprTempMean,mayTempMean,junTempMean,julTempMean,augTempMean,sepTempMean,octTempMean,novTempMean,decTempMean]
-    ax.bar(months,avgTemps)
+    ax = fig.add_axes([0.1,0.1,0.8,0.8])    
+    ax.bar(months,avgTemps,color='red')
     plt.xlabel("x - Month")
     plt.ylabel("y - Temprature (°C)")
     plt.title("Pakistan's Average Temprature/Month from 1901-2016")
@@ -117,12 +120,26 @@ def avgTMG():
 def avgRMG():
     fig = plt.figure()
     ax = fig.add_axes([0.1,0.1,0.8,0.8])
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    avgRain = [janRainMean,febRainMean,marRainMean,aprRainMean,mayRainMean,junRainMean,julRainMean,augRainMean,sepRainMean,octRainMean,novRainMean,decRainMean]
-    ax.bar(months,avgRain)
+    ax.bar(months,avgRain,color='green')
     plt.xlabel("x - Month")
     plt.ylabel("y - Rainfall (mm)")
     plt.title("Pakistan's Average Rainfall/Month from 1901-2016")
+    plt.show()
+
+# Twin Axes on Mean Rain and Temprature Relation
+def TARTMG():
+    fig = plt.figure()    
+    
+    a1 = fig.add_axes([0.1,0.1,0.8,0.8])    
+    a1.plot(avgTemps, 'go-')
+    #a1.set_xlabel('Months')
+    a1.set_ylabel('Temp (°C)')
+    
+    a2 = a1.twinx()
+    a2.plot(avgRain, 'ro-')
+    a2.set_ylabel('Rain (mm)')
+
+    fig.legend(labels = ('Temp (°C)','Rain (mm)'),loc='upper center')
     plt.show()
 
 # Rainfall on X, Temprature on Y, Bar Graph, Shows a trend.
@@ -138,16 +155,20 @@ def scatterGraphFunc():
 #distribution -- analysis
 #regression -- prediction
 
+
+### Function Calling
+
 # meanTempRainFunc()
 # medianTempRainFunc()
 # dataDescript()
 # minMaxTempratureRow()
 # minMaxRainfallRow()
-avgTMG()
-avgRMG()
+# avgTMG()
+# avgRMG()
+TARTMG()
 # barGraphFunc()
 # scatterGraphFunc()
 
 #
-print("\n\n") # this is the end of  main code. 
+print("\n") # this is the end of  main code. 
 #
