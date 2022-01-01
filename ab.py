0e630680a0455924a773ce664e74e5420dd224fd
DanieiUni <f190931@cfd.nu.edu.pk>
2022-01-01 15:04:20 +0500

    added basic UI

--- ab.py
+++ ab.py
@@ -1,22 +1,10 @@
+from tkinter import *
+from ab3 import scatterGraphFunc
+root = Tk(className="Python Rainfall Graphs")
+def onButtonClick():
+    scatterGraphFunc()
 
-print("\n\n\n") # this code only initializes tkinter python 
-
-import tkinter as tk
-
-class App(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-
-# create the application
-myapp = App()
-
-#
-# here are method calls to the window manager class
-#
-myapp.master.title("My Do-Nothing Application")
-myapp.master.maxsize(1000, 400)
-
-# start the program
-myapp.mainloop()
+functionButton = Button(root,text="Click for graph",command=onButtonClick).pack()
+root.geometry("600x400")
+root.mainloop()
 
